0c956f784824c709ad70b6f1198350e6ed3908bf
treeoflife321 <144504577+treeoflife321@users.noreply.github.com>
2024-11-07 01:23:33 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,50 +1,52 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Ubuntu Server PHP Deployment</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubuntu Server PHP Deployment</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<link rel="stylesheet" href="style.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
 <body>
-	<h1>Ubuntu Server PHP Deployment-SCOTT_PAUROM</h1>
+    <div class="container">
+        <h1>Ubuntu Server PHP Deployment - SCOTT_PAUROM</h1>
+        
+        <!-- Form inputs -->
+        <div class="mb-3">
+            <label for="firstname" class="form-label">Firstname</label>
+            <input type="text" name="firstname" id="firstname" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label for="middlename" class="form-label">Middlename</label>
+            <input type="text" name="middlename" id="middlename" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label for="lastname" class="form-label">Lastname</label>
+            <input type="text" name="lastname" id="lastname" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label for="age" class="form-label">Age</label>
+            <input type="text" name="age" id="age" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label for="address" class="form-label">Address</label>
+            <input type="text" name="address" id="address" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label for="courseandsection" class="form-label">Course & Section</label>
+            <input type="text" name="courseandsection" id="courseandsection" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
-
-<div class="container">
-	<label>Firstname</label>
-	<input type="text" name="firstname" class= "form-control">
-</div>
-
-<div class="container">
-	<label>Middlename</label>
-	<input type="text" name="middlename" class= "form-control">
-</div>
-
-<div class="container">
-	<label>Lastname</label>
-	<input type="text" name="lastname" class= "form-control">
-</div>
-
-<div class="container">
-	<label>Age</label>
-	<input type="text" name="age" class= "form-control">
-</div>
-
-<div class="container">
-	<label>Address</label>
-	<input type="text" name="address" class= "form-control">
-</div>
-
-<div class="container">
-	<label>Course & Section</label>
-	<input type="text" name="courseandsection" class= "form-control">
-</div>
-
-<div class="container">
-	<button>Submit</button>
-</div>
-
 </html>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
